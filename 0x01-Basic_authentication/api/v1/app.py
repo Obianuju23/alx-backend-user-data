cb2c30acd267931d4ec7ddd5bbb6c8fdb61b763a
Obianuju23 <obianujunmoh@gmail.com>
2023-11-07 22:56:03 +0100

    updated app.py with task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -41,6 +41,27 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
+
+@app.before_request
+def before_request():
+    """Method to handler before request"""
+
+    if auth is None:
+        return
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
