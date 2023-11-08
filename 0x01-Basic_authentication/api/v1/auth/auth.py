0e84addd1ec6c940d191cacd425e42fc932b666e
Obianuju23 <obianujunmoh@gmail.com>
2023-11-08 04:15:15 +0100

    I updated the auth file

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,7 +4,7 @@ from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
+class Auth():
     """This class gives Public method definition of class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """It returns False if path and excluded paths"""
