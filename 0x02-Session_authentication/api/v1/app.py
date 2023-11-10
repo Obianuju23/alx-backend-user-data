34bb85cf5e03d77220f0287c81c0796142863afa
Obianuju23 <obianujunmoh@gmail.com>
2023-11-10 03:53:51 +0100

    updated the app files

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -62,6 +63,8 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
