beba76404bd1c1398200d6f14e76eabaf2a7f1ef
Obianuju23 <obianujunmoh@gmail.com>
2023-11-10 04:15:29 +0100

    updated the app files

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
