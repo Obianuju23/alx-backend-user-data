22f04f51e0217e5565c5d63d0830465fc667e316
Obianuju23 <obianujunmoh@gmail.com>
2023-11-12 23:12:19 +0100

    added cookie session

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -34,7 +34,7 @@ class Auth():
 
         return None
 
-     def session_cookie(self, request=None):
+    def session_cookie(self, request=None):
         """ returns a cookie value from a request """
         if request is None:
             return None
