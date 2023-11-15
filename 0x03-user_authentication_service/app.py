dc386b710207b329d8c6efe263a21536749e94bf
Obianuju23 <obianujunmoh@gmail.com>
2023-11-15 11:50:23 +0100

    updating app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,5 +14,18 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"])
+def users() -> str:
+    """ register a user to database """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        msg = {"email": email, "message": "user created"}
+        return jsonify(msg)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
