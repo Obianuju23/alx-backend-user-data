8bb34b418626460fc01f053c1525828b7239638a
Obianuju23 <obianujunmoh@gmail.com>
2023-11-15 10:56:14 +0100

    added auth file that returns salted hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,6 @@ from typing import Union
 def _hash_password(self, password: str) -> bytes:
     """returns salted hash of the input password, hashed with bcrypt.hashpw"""
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
